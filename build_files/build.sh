99fc12479a2d0b051a6a37fdb1daaa66710c877a
Ruby Dennington <theleruby@gmail.com>
2026-02-28 17:37:33 +0000

    opt fixes, more packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,6 +2,9 @@
 
 set -ouex pipefail
 
+# make sure /var/opt directory exists so we can install stuff into it
+mkdir -p /var/opt
+
 # make sure all the fedora repo definitions are installed as bazzite doesn't include these
 dnf5 install -y fedora-workstation-repositories
 
@@ -25,11 +28,11 @@ dnf5 install -y java-21-openjdk-devel.x86_64 java-25-openjdk-devel.x86_64
 
 # chrome
 dnf5 config-manager setopt google-chrome.enabled=1
-#dnf5 install -y google-chrome-stable
+dnf5 install -y google-chrome-stable
 
 # docker
 dnf5 config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo
-#dnf5 install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+dnf5 install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # .net
 dnf5 install -y dotnet-sdk-8.0 dotnet-runtime-8.0
@@ -42,7 +45,7 @@ dnf5 install -y nodejs
 dnf5 install -y mercurial tortoisehg python3-dulwich
 
 # mysql
-dnf5 install -y mariadb-devel
+dnf5 install -y mariadb-devel mariadb-connector-c-doc
 
 # imagemagick
 dnf5 install -y ImageMagick-devel
@@ -51,7 +54,7 @@ dnf5 install -y ImageMagick-devel
 dnf5 install -y fluidsynth
 
 # vlc
-dnf5 install -y vlc vlc-plugin-fluidsynth vlc-plugin-kde
+dnf5 install -y vlc vlc-plugin-ffmpeg vlc-plugin-fluidsynth vlc-plugin-kde vlc-plugin-visualization
 
 # yt-dlp
 dnf5 install -y yt-dlp
@@ -64,3 +67,23 @@ dnf5 install -y libreoffice
 
 # discord
 dnf5 install -y discord
+
+# audacity
+dnf5 install -y audacity-freeworld
+
+# krusader
+dnf5 install -y krusader
+
+# move stuff in /var/opt to /usr/lib/opt and add symlink to tmpfiles conf
+# taken from https://github.com/astrovm/amyos/blob/main/build_files/fix-opt.sh, thanks <3
+for dir in /var/opt/*/; do
+  [ -d "$dir" ] || continue
+  dirname=$(basename "$dir")
+  mv "$dir" "/usr/lib/opt/$dirname"
+  echo "L+ /var/opt/$dirname - - - - /usr/lib/opt/$dirname" >>/usr/lib/tmpfiles.d/move-opt-files.conf
+done
+
+# enable docker
+systemctl enable docker.service containerd.service
+
+dnf5 clean all
