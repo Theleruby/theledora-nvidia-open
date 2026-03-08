9e3d28b20227c95da60199336ada59695ed76317
Ruby Dennington <theleruby@gmail.com>
2026-03-08 10:15:49 +0000

    Update vlc plugin packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -57,7 +57,7 @@ dnf5 install -y ImageMagick-devel
 dnf5 install -y fluidsynth fluid-soundfont-common fluid-soundfont-gm
 
 # vlc
-dnf5 install -y vlc vlc-plugin-ffmpeg vlc-plugin-fluidsynth vlc-plugin-kde vlc-plugin-visualization vlc-plugin-notify
+dnf5 install -y vlc vlc-plugin-ffmpeg vlc-plugin-fluidsynth vlc-plugin-kde vlc-plugin-visualization vlc-plugin-notify vlc-plugin-pipewire vlc-plugins-freeworld
 
 # yt-dlp
 dnf5 install -y yt-dlp
